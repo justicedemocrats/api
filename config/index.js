5929ccef73619e1ca92781d41794c8c58c4ae589
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-11-11 09:00:27 -0500

    use production instead of prod

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
-if (process.env.NODE_ENV == "prod") {
-  module.exports = require("./prod.js");
+if (process.env.NODE_ENV == "production") {
+  module.exports = require("./production.js");
 } else {
   module.exports = require("./dev.js");
 }
