27c71127605c04ad295c1059576b4bc3c1bf9427
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-11-17 16:09:49 -0500

    its all caps

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 if (process.env.USE_ENV) {
   const result = process.env;
-  result.templates = require("./templates");
+  result.TEMPLATES = require("./templates");
   module.exports = result;
 } else if (process.env.NODE_ENV == "production") {
   module.exports = require("./production.js");
