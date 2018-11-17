3eb3cadc9372bd23236d01afd2f82c40b91d5621
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-11-17 16:18:08 -0500

    correctly require templates

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 if (process.env.USE_ENV) {
-  const result = process.env;
+  const result = Object.assign({}, process.env);
   result.TEMPLATES = require("./templates");
+  console.log(result.TEMPLATES);
   module.exports = result;
 } else if (process.env.NODE_ENV == "production") {
   module.exports = require("./production.js");
