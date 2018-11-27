95be7387e7c03ad6953eadc506ece21adeaf35c7
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-11-27 10:33:15 -0500

    fix upsert person quirks – accept just a phone and name, fix search for name, search for name when no emails or phones are found

--- config/index.js
+++ config/index.js
@@ -1,7 +1,6 @@
 if (process.env.USE_ENV) {
   const result = Object.assign({}, process.env);
   result.TEMPLATES = require("./templates");
-  console.log(result.TEMPLATES);
   module.exports = result;
 } else if (process.env.NODE_ENV == "production") {
   module.exports = require("./production.js");
