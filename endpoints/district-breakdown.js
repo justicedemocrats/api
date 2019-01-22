759e3d628b0d60d635117af5bdd40e7e36087c5d
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2019-01-22 09:23:47 -0500

    include previous cycle numbers

--- endpoints/district-breakdown.js
+++ endpoints/district-breakdown.js
@@ -1,13 +1,27 @@
 const db = require("../lib/db");
 const knex = require("knex");
+const previousCycle = require("./data/2018-breakdown");
 
 async function main(req, res) {
+  const hidePreviousCycle = req.query.hidePreviousCycle;
+
   const breakdown = await db("nominations")
     .select(`state`, `district`)
     .count("*")
     .groupBy("state", "district");
 
-  return res.json(breakdown);
+  return res.json(
+    hidePreviousCycle ? breakdown : mapSum(breakdown, previousCycle)
+  );
 }
 
 module.exports = main;
+
+function mapSum(map1, map2) {
+  const result = {};
+  const keys = [...new Set(Object.keys(map1).concat(Object.keys(map2)))];
+  for (let key of keys) {
+    result[key] = (map1[key] || 0) + (map2[key] || 0);
+  }
+  return result;
+}
